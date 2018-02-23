38e52395780931f2fa24b4f89fd171b1a81f463f
Zoey Zheng <zoeyyandi@gmail.com>
2018-02-23 11:27:31 -0500

    edited styles

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(function() {
+  ///// variables
   var controller = new ScrollMagic.Controller({
     globalSceneOptions: {
       triggerHook: 'onLeave'
@@ -7,6 +8,9 @@ $(function() {
 
   // get all slides
   var slides = document.querySelectorAll('.scene');
+
+  // video slide
+  var videoSlide = $('.dispatch-video')[0];
 
   // create scene for every slide
   slides.forEach(function(slide) {
@@ -22,52 +26,61 @@ $(function() {
   });
 
   // building scenes: toggle css classes for elements
-  new ScrollMagic.Scene({ triggerElement: '#video' })
-    .setClassToggle('#side-nav-logo', 'active')
-    .addTo(newController);
-  new ScrollMagic.Scene({ triggerElement: '#about', triggerHook: 0.25 })
-    .setClassToggle('#high2', 'active')
-    .addTo(newController);
-  new ScrollMagic.Scene({ triggerElement: '#portfolio' })
-    .setClassToggle('#high3', 'active')
-    .addTo(newController);
+  // new ScrollMagic.Scene({ triggerElement: '#video' })
+  //   .setClassToggle('#side-nav-logo', 'active')
+  //   .addTo(newController);
+  // new ScrollMagic.Scene({ triggerElement: '#about', triggerHook: 0.25 })
+  //   .setClassToggle('#high2', 'active')
+  //   .addTo(newController);
+  // new ScrollMagic.Scene({ triggerElement: '#portfolio' })
+  //   .setClassToggle('#high3', 'active')
+  //   .addTo(newController);
   new ScrollMagic.Scene({ triggerElement: '#portfolio2' })
-    .setClassToggle('#high3', 'active')
-    .addTo(newController);
-  new ScrollMagic.Scene({ triggerElement: '#portfolio3' })
-    .setClassToggle('#high3', 'active')
-    .addTo(newController);
-  new ScrollMagic.Scene({ triggerElement: '#services' })
-    .setClassToggle('#high4', 'active')
-    .addTo(newController);
-  new ScrollMagic.Scene({ triggerElement: '#contact' })
-    .setClassToggle('#high5', 'active')
-    .addTo(newController);
-  new ScrollMagic.Scene({ triggerElement: '#about', triggerHook: 0.75 })
-    .setClassToggle('.contactus', 'active2')
-    .addIndicators()
-    .addTo(newController);
-  new ScrollMagic.Scene({ triggerElement: '#about', triggerHook: 0.6 })
-    .setClassToggle('.servicesoffered', 'active2')
-    .addIndicators()
-    .addTo(newController);
-  new ScrollMagic.Scene({ triggerElement: '#about', triggerHook: 0.42 })
-    .setClassToggle('.portfolioprojects', 'active2')
-    .addIndicators()
-    .addTo(newController);
-  new ScrollMagic.Scene({ triggerElement: '#about', triggerHook: 0.25 })
-    .setClassToggle('.aboutus', 'active2')
-    .addIndicators()
-    .addTo(newController);
+    .on('leave', function() {
+      videoSlide.trigger('pause');
+    })
+    .on('enter', function() {
+      var slideNo = $(this).superslides('current');
+      if (slideNo === 3) {
+        videoSlide.play();
+      }
+    });
+  //   .setClassToggle('#high3', 'active')
+  //   .addTo(newController);
+  // new ScrollMagic.Scene({ triggerElement: '#portfolio3' })
+  //   .setClassToggle('#high3', 'active')
+  //   .addTo(newController);
+  // new ScrollMagic.Scene({ triggerElement: '#services' })
+  //   .setClassToggle('#high4', 'active')
+  //   .addTo(newController);
+  // new ScrollMagic.Scene({ triggerElement: '#contact' })
+  //   .setClassToggle('#high5', 'active')
+  //   .addTo(newController);
+  // new ScrollMagic.Scene({ triggerElement: '#about', triggerHook: 0.75 })
+  //   .setClassToggle('.contactus', 'active2')
+  //   .addIndicators()
+  //   .addTo(newController);
+  // new ScrollMagic.Scene({ triggerElement: '#about', triggerHook: 0.6 })
+  //   .setClassToggle('.servicesoffered', 'active2')
+  //   .addIndicators()
+  //   .addTo(newController);
+  // new ScrollMagic.Scene({ triggerElement: '#about', triggerHook: 0.42 })
+  //   .setClassToggle('.portfolioprojects', 'active2')
+  //   .addIndicators()
+  //   .addTo(newController);
+  // new ScrollMagic.Scene({ triggerElement: '#about', triggerHook: 0.25 })
+  //   .setClassToggle('.aboutus', 'active2')
+  //   .addIndicators()
+  //   .addTo(newController);
 
-  new ScrollMagic.Scene({ triggerElement: '#portfolio' })
-    .setClassToggle('.listOfContents', 'active2')
-    // .addIndicators()
-    .addTo(newController);
-  new ScrollMagic.Scene({ triggerElement: '#services' })
-    .setClassToggle('.listOfContents', 'active2')
-    // .addIndicators()
-    .addTo(newController);
+  // new ScrollMagic.Scene({ triggerElement: '#portfolio' })
+  //   .setClassToggle('.listOfContents', 'active2')
+  //   // .addIndicators()
+  //   .addTo(newController);
+  // new ScrollMagic.Scene({ triggerElement: '#services' })
+  //   .setClassToggle('.listOfContents', 'active2')
+  //   // .addIndicators()
+  //   .addTo(newController);
   new ScrollMagic.Scene({ triggerElement: '#contact' })
     .on(
       'update',
@@ -91,9 +104,26 @@ $(function() {
     }
   }
 
-  $('#slides-1, #slides-2, #slides-3').superslides({
+  $('#slides-1, #slides-3').superslides({
     hashchange: true,
     pagination: true,
-    play: 5000
+    play: 2000
+  });
+
+  $('#slides-2').superslides({
+    hashchange: true,
+    pagination: true,
+    play: 2000
+  });
+
+  $('#slides-2').on('animated.slides', function() {
+    var slideNo = $(this).superslides('current');
+    if (slideNo === 3) {
+      $(this).superslides('stop');
+      videoSlide.play();
+      videoSlide.on('ended', function() {
+        $('#slides-2').superslides('start');
+      });
+    }
   });
 });
